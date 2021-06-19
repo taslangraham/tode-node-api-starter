1a77bccdc2543a643f003726d44212289a459ad7
taslangraham <tgraham@realdecoy.com>
2021-06-19 17:29:16 -0500

    added default export to entity

--- src/models/index.entity.ts
+++ src/models/index.entity.ts
@@ -1,1 +1,6 @@
 export * from './example';
+
+// MikrORM expects something to be exported from this file
+// This is important if you currently have no Model and want to create migrations
+// Withou this the migration command will throw an error
+export default {};
