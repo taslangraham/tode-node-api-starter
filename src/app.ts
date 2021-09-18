16f2ab6472be3ea7acaac2f0a19a30dd7a79266f
taslangraham <tgraham@realdecoy.com>
2021-09-18 01:20:23 -0500

    update

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import Knex from 'knex';
 import { Model } from 'objection';
 import knexConfig from '../knexfile';
+import { loadBodyParser } from "./config";
 import { config as databaseConfig } from "./config/database/db-config";
 import { routeTable } from "./config/route-table";
 import { loadRoutes } from "./controllers";
@@ -15,27 +16,29 @@ Model.knex(knex);
 const app = express();
 
 try {
-  (async () => {
-    app.get("/", async (req: Request, res: Response) => {
-      return res.status(200).send({
-        message: `Powered by Tode - a Nodejs Scaffolding told.\nBelow is a list of your Application's endpoints`,
-        endpoints: routeTable.routes,
-      },
-      );
-    });
+  loadBodyParser(app);
+  // Load configuration settings for Database
+  // You can define the values in env.ts
 
-    // register the entry route above
-    routeTable.register(app._router.stack
-      .filter((s: { route: any; }) => s.route)
-      .map((r: any) => ({
-        name: '/',
-        path: r.route.path,
-        method: Object.keys(r.route.methods)[0],
-      })));
+  app.get("/", async (req: Request, res: Response) => {
+    return res.status(200).send({
+      message: `Powered by Tode - a Nodejs Scaffolding told.\nBelow is a list of your Application's endpoints`,
+      endpoints: routeTable.routes,
+    },
+    );
+  });
 
-    // Loads in user defined routes
-    loadRoutes(app);
-  })();
+  // register the entry route above
+  routeTable.register(app._router.stack
+    .filter((s: { route: any; }) => s.route)
+    .map((r: any) => ({
+      name: '/',
+      path: r.route.path,
+      method: Object.keys(r.route.methods)[0],
+    })));
+
+  // Loads in user defined routes
+  loadRoutes(app);
 } catch (error) {
   throw new Error(error as string);
 }
