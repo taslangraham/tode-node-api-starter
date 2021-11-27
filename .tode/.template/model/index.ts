c2bf17c6f54c5eccc0ebf862a4cfe3cfe4c67e3e
Taslan A. Graham <49318261+taslangraham@users.noreply.github.com>
2021-11-27 17:44:01 -0500

    update model (#1)
    Co-authored-by: taslangraham <tgraham@realdecoy.com>

--- .tode/.template/model/index.ts
+++ .tode/.template/model/index.ts
@@ -1,12 +1,17 @@
 import BaseModel from "../BaseMode";
 
 export class ModelClassName extends BaseModel {
+  // Name of table that this model maps back to
   // Table name is the only required property.
-  public static tableName = 'animals';
+  public static tableName = 'ENTER_TABLE_NAME';
+  // Example property
   public foo!: string;
+
+  // Add other tbale fields (columns) as properties to access them via the model
 
   // This object defines the relations to other models. The relationMappings
   // property can be a thunk to prevent circular dependencies.
+  // READ MORE at https://vincit.github.io/objection.js/guide/relations.html
   public static relationMappings = () => ({
     // specify relation with other modules
   })
