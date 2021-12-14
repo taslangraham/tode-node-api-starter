dd1988e3171f6fd5a090703c2aed7ac081b2854d
taslangraham <tgraham@realdecoy.com>
2021-12-13 21:45:30 -0500

    update comment

--- src/config/database/db-config.ts
+++ src/config/database/db-config.ts
@@ -18,8 +18,7 @@ const connnection = {
 function initializeDatabase() {
 
   // Initialize knex.
-  // Bind all Models to a knex instance. If you only have one database in
-  // your server this is all you have to do. For multi database systems, see
+  // Bind all Models to a knex instance.
   if (env.database.isEnabled) {
     const knex = Knex(connnection);
     Model.knex(knex);
