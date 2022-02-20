60f8b9f57bb2e2ca1d2316bb2ee996a7063847d6
taslangraham <tgraham@realdecoy.com>
2022-02-20 15:32:24 -0500

    update auth middleware

--- .tode/.template/auth/middlewares/auth.ts
+++ .tode/.template/auth/middlewares/auth.ts
@@ -1,31 +1,32 @@
-import { NextFunction, Request, Response } from 'express';
 import { authService } from '../services/auth';
+import { NextFunction } from "express"
+import { Request, Response } from '../config/core'
 
-/**
- * Middleware that verifies if a request has a valid token
- * @param req
- * @param response
- * @param next
- */
-export const isLoggedIn = (req: Request, res: Response, next: NextFunction) => {
-  const authToken = req.headers.authorization && req.headers.authorization;
-  try {
+export default class Auth {
+  public handler(request: Request, response: Response, next: NextFunction) {
+    const authToken = request.headers.authorization && request.headers.authorization;
+    try {
 
-    if (!authToken) {
-      return res.status(403).send({ auth: false, message: 'Missing required Token' });
+      if (!authToken) {
+        return response
+          .status(403)
+          .send({ auth: false, message: 'Missing required Token' });
+      }
+
+      const decoded = authService.decodeToken(authToken);
+      request.Auth = {
+        id: decoded.id,
+        email: decoded.email,
+        firstName: decoded.firstName,
+        lastName: decoded.lastName,
+      };
+
+      return next();
+    } catch (error) {
+      console.log(`[ Auth middleware Error ]: ${error}`);
+      return response
+        .status(403)
+        .send({ auth: false, message: JSON.stringify(error) });
     }
-
-    const decoded = authService.decodeToken(authToken);
-    req.body._user = {
-      id: decoded.id,
-      email: decoded.email,
-      firstName: decoded.firstName,
-      lastName: decoded.lastName,
-    };
-
-    return next();
-  } catch (error) {
-    console.log(`[ Auth middleware Error ]: ${error}`);
-    return res.status(403).send({ auth: false, message: JSON.stringify(error) });
   }
-};
+}
