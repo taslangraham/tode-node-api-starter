26ced7403903ab587c34f3c4db9b9ab5edee6753
taslangraham <tgraham@realdecoy.com>
2022-03-10 20:23:28 -0500

    update formatting

--- .tode/.template/auth/services/auth/index.ts
+++ .tode/.template/auth/services/auth/index.ts
@@ -7,86 +7,86 @@ const TOKEN_TIME_TO_LIVE = 86400; // 24 hours
 const { JWT_SECRET } = env;
 
 export interface LoginInfo {
-	email: string;
-	password: string;
+  email: string;
+  password: string;
 }
 
 interface JwtDecode extends JwtPayload {
-	id: string;
-	firstName: string;
-	lastName: string;
-	email: string;
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
 }
 
 class Auth {
 
-	constructor() {
-		//
-	}
+  constructor() {
+    //
+  }
 
-	/**
-	 * Creates a JSON web token
-	 * @param user
-	 * @returns
-	 */
-	public createTokenFromUser(user: User) {
-		const body = {
-			id: user && user.$id(),
-			firstName: user && user.firstName,
-			email: user && user.email,
-			lastName: user && user.lastName,
-		};
+  /**
+   * Creates a JSON web token
+   * @param user
+   * @returns
+   */
+  public createTokenFromUser(user: User) {
+    const body = {
+      id: user && user.$id(),
+      firstName: user && user.firstName,
+      email: user && user.email,
+      lastName: user && user.lastName,
+    };
 
-		return jwt.sign(
-			body,
-			JWT_SECRET,
-			{ expiresIn: TOKEN_TIME_TO_LIVE },
-		);
-	}
+    return jwt.sign(
+      body,
+      JWT_SECRET,
+      { expiresIn: TOKEN_TIME_TO_LIVE },
+    );
+  }
 
-	/**
-	 * Decodes a JSON web token
-	 * @param token
-	 * @returns
-	 */
-	public decodeToken(token: string) {
-		return jwt.verify(token, JWT_SECRET, {
-			// add additional options here
-		}) as JwtDecode;
-	}
+  /**
+   * Decodes a JSON web token
+   * @param token
+   * @returns
+   */
+  public decodeToken(token: string) {
+    return jwt.verify(token, JWT_SECRET, {
+      // add additional options here
+    }) as JwtDecode;
+  }
 
-	/**
-	 * Validates a User's login credentials and return the User if found
-	 * @param credentials
-	 */
-	public async login(credentials: LoginInfo) {
-		let result: ServiceResponse<User> = { success: false };
-		try {
-			const user = await User.query().findOne({ email: credentials.email }) || null;
+  /**
+   * Validates a User's login credentials and return the User if found
+   * @param credentials
+   */
+  public async login(credentials: LoginInfo) {
+    let result: ServiceResponse<User> = { success: false };
+    try {
+      const user = await User.query().findOne({ email: credentials.email }) || null;
 
-			if (user === null) {
-				result = {
-					success: true,
-					data: user,
-				};
-			} else {
-				const isCorrectPassword = await hashCompare(credentials.password, user.password);
+      if (user === null) {
+        result = {
+          success: true,
+          data: user,
+        };
+      } else {
+        const isCorrectPassword = await hashCompare(credentials.password, user.password);
 
-				if (isCorrectPassword) {
-					result = {
-						success: true,
-						data: user,
-					};
-				}
+        if (isCorrectPassword) {
+          result = {
+            success: true,
+            data: user,
+          };
+        }
 
-			}
-		} catch (error) {
-			console.log(error);
-			throw new Error('Failed to login');
-		}
+      }
+    } catch (error) {
+      console.log(error);
+      throw new Error('Failed to login');
+    }
 
-		return result;
-	}
+    return result;
+  }
 }
 
 const authService = new Auth();
